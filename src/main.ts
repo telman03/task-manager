91429ae620040aaa87a448d8f09a3f9e8d6ac8a8
telman03 <telmangadimov1@gmail.com>
2023-11-14 15:57:39 +0400

    Update start script to use compiled code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
